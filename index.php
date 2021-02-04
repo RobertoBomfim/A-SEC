c4fd22af1632d7a0d27eb0a49902e86da8e6f4b9
RobertoBomfim <bomfim062@gmail.com>
2021-02-04 11:37:39 -0300

    First commit

---
+++ index.php
@@ -0,0 +1,477 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+
+<head>
+
+	<meta name="robots" content="index, follow">
+	<meta charset="utf-8">
+	<meta https-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Higienização de Estofados a Seco">
+	<meta name="keywords" content="higienização,lavagem,estofados,oxi-sanitização,sofá,colchões,veículos, impermeabilização,salvador,bahia,brasil">
+	<meta name="author" content="A ` Sec">
+	  
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>A ` Sec - Higienização de Estofados</title>
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round" rel="stylesheet">
+
+	<!-- Bootstrap -->
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
+
+	<!-- Magnific Popup -->
+	<link type="text/css" rel="stylesheet" href="css/magnific-popup.css" />
+
+	<!-- Font Awesome Icon -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="css/style.css" />
+
+	<style type="text/css">
+		.color-marca{
+			color:;
+		}
+
+		a { color: inherit; } 
+		
+	</style>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+
+
+<body>
+
+
+	<!-- Header -->
+	<header id="home">
+
+		<!-- Background Image -->
+			<div class="bg-img" style="background-image: url('./img/background1.png');"></div>
+		<!-- /Background Image -->
+
+		<!-- Nav -->
+		<nav id="nav" class="navbar nav-transparent">
+			<div class="container">
+
+				<div class="navbar-header">
+					<!-- Logo -->
+					<div class="navbar-brand">
+						<a href="index.php">
+							<img class="logo" src="img/logo1.png" alt="logo">
+							<img class="logo-alt" src="img/logo.png" alt="logo-alt">
+						</a>
+					</div>
+					<!-- /Logo -->
+				</div>
+
+				<!--  Main navigation  -->
+				<ul class="main-nav nav navbar-nav navbar-right">
+					<li><a href="#home">Início</a></li>
+					<li><a href="#service">Serviços</a></li>
+					<li><a href="#sobre">Sobre nós</a></li>
+					<li><a href="#galeria">Galeria</a></li>
+					<li><a href="#contact">Contato</a></li>
+				</ul>
+				<!-- /Main navigation -->
+			</div>
+		</nav>
+		<!-- /Nav -->
+
+		<!-- home wrapper -->
+		<div class="home-wrapper">
+			<div class="container">
+				<div class="row">
+
+					<!-- home content -->
+					<div class="col-md-10 col-md-offset-1">
+						<div class="home-content">
+							<h1 class="white-text" >Marque agora uma higienização e deixe seu estofado como novo!</h1>
+						</div>
+					</div>
+					<!-- /home content -->
+
+				</div>
+			</div>
+		</div>
+		<!-- /home wrapper -->
+
+	</header>
+	<!-- /Header -->
+
+
+	<!-- Service -->
+	<div id="service" class="section md-padding">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Section header -->
+				<div class="section-header text-center">
+					<h2 class="title">Serviços</h2>
+				</div>
+				<!-- /Section header -->
+
+				<!-- service -->
+				<div class="col-md-4 col-sm-6">
+					<div class="service">
+						<i class="fa fa-tint"></i>
+						<h4>Higienização de Sofás</h4>
+						<p>Limpeza do sofá, com aplicação do flotador para a retirada da sujidade, juntamente com peróxido de carbono para eliminação dos ácaros, fungos e bactérias que estão no estofado.</p>
+						<br><br>
+					</div>
+				</div>
+				<!-- /service -->
+
+				<!-- service -->
+				<div class="col-md-4 col-sm-6">
+					<div class="service">
+						<i class="fa fa-tint"></i>
+						<h4>Impermeabilização</h4>
+						<p>Serviço de blindagem do seu estofado. Repele líquidos em geral, ideal para acidentes que envolvem derramamento de sucos, cafés e outros.</p>
+						<br><br><br>
+					</div>
+				</div>
+				<!-- /service -->
+
+				<!-- service -->
+				<div class="col-md-4 col-sm-6">
+					<div class="service">
+						<i class="fa fa-tint"></i>
+						<h4>Higienização de Colchões</h4>
+						<p>Serviço de limpeza total do seu colchão, onde é feita a eliminação dos ácaros, fungos e bactérias que permanecem na superfície do colchão, e podem causar danos a nossa saúde, como coceiras e problemas respiratórios.</p>
+					</div>
+				</div>
+				<!-- /service -->
+				
+			</div>
+			<!-- /Row -->
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- service -->
+				 <div class="col-md-4 col-sm-6" > 
+					<div class="service">
+						<i class="fa fa-tint"></i>
+						<h4>Higienização de Veículos</h4>
+						<p>Processo de limpeza do interior de automóveis, envolvendo bancos, teto, painel, cintos, partes plásticas e carpetes. Através de limpeza minuciosa, toda parte interna do veículo é renovada.</p>
+						<br>
+					</div>
+				</div>
+				<!-- /service -->
+
+				<!-- service -->
+				<div class="col-md-4 col-sm-6">
+					<div class="service">
+						<i class="fa fa-tint"></i>
+						<h4>Limpeza de tapetes</h4>
+						<p>Aspiração e lavagem de tapetes residenciais, com eliminação de microrganismos que se alojam no tecido.</p>
+						<br><br><br><br>
+					</div>
+				</div>	
+				<!-- /service -->
+
+				<!-- service -->
+				<div class="col-md-4 col-sm-6">
+					<div class="service">
+						<i class="fa fa-tint"></i>
+						<h4>Oxi-sanitização</h4>
+						<p>Serviço  de eliminação de odores, bactérias, fungos e outros microrganismos que estejam  alojados principalmente no interior de veículos.</p>
+						<br><br><br>
+					</div>
+				</div>	
+				<!-- /service -->
+
+			</div>
+			<!-- /Row -->
+
+			<div class="pagamento">
+					<h5 class="fa fa-credit-card"></h5>
+					<h5>Aceitamos cartões de débito e crédito.</h5>
+			</div>
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- /Service -->
+
+
+	<!-- About -->
+	<div id="sobre" class="section md-padding bg-dark">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Section header -->
+				<div class="section-header text-center">
+					<h2 class="title" style="color: #FAFAFA">Sobre nós</h2>
+					
+					<p>A ` Sec é uma jovem empresa que atua no ramo de higienização, levando até a residência de nossos clientes parceiros o bem-estar, com o intuito de lhes proporcionar satisfação em escolher confiar nos nossos serviços. Temos a intenção de sempre fortalecer o vínculo com nossos clientes, oferecendo serviços de excelência com preço justo e qualidade.</p>
+					<h3 class="title" style="color: #FAFAFA">Missão</h3>
+					<p>Fornecer bem-estar e saúde aos nossos clientes e seus familiares, e ter reconhecimento da excelência dos nossos serviços.</p>
+					<h3 class="title" style="color: #FAFAFA">Visão</h3>
+					<p>Consolidar nossa marca e nos tornar referência em Salvador na qualidade do ramo de higienização de estofados.</p>
+					<h3 class="title" style="color: #FAFAFA">Valores</h3>
+					<p>Comprometimento, Parceria, Respeito, Transparência.</p>
+					<br></br>
+					<!-- <h5 style="color: #6195FF"><a href="" target="_blank">Assista nosso video de apresentação</a></h5> -->
+
+				</div>
+				<!-- /Section header -->
+
+			</div>		
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- /About -->
+
+
+	<!-- Portfolio -->
+	<div id="galeria" class="section md-padding">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Section head-->				
+				<div class="section-header text-center">
+					<h2 class="title">Clientes</h2>	
+				</div>
+				<!-- /Section header -->
+				
+				<!-- Work -->
+				<div class="col-md-4 col-xs-6 work">
+					<img class="img-responsive" src="./img/cliente1.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>Cliente Clara</h3>
+						<div class="work-link">
+							<a class="lightbox" href="./img/cliente1.jpg"><i class="fa fa-search"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- /Work -->
+
+				<!-- Work -->
+				<div class="col-md-4 col-xs-6 work">
+					<img class="img-responsive" src="./img/cliente2.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>Cliente Larissa</h3>
+						<div class="work-link">
+							<a class="lightbox" href="./img/cliente2.jpg"><i class="fa fa-search"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- /Work -->
+
+				<!-- Work -->
+				<div class="col-md-4 col-xs-6 work">
+					<img class="img-responsive" src="./img/cliente3.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>Cliente Débora</h3>
+						<div class="work-link">
+							<a class="lightbox" href="./img/cliente3.jpg"><i class="fa fa-search"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- /Work -->
+
+			</div>
+
+			<div class="row">
+
+				<!-- Work -->
+				<div class="col-md-4 col-xs-6 work">				
+					<img class="img-responsive" src="./img/cliente4.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>Cliente Kátia</h3>
+						<div class="work-link">
+							<a class="lightbox" href="./img/cliente4.jpg"><i class="fa fa-search"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- /Work -->
+
+				<!-- Work -->
+				<div class="col-md-4 col-xs-6 work">
+					<img class="img-responsive" src="./img/cliente5.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>Cliente Mariana</h3>
+						<div class="work-link">
+							<a class="lightbox" href="./img/cliente5.jpg"><i class="fa fa-search"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- /Work -->
+
+				<!-- Work -->
+				<div class="col-md-4 col-xs-6 work">
+					<img class="img-responsive" src="./img/cliente6.jpg" alt="">
+					<div class="overlay"></div>
+					<div class="work-content">
+						<h3>Cliente Mônica</h3>
+						<div class="work-link">
+							<a class="lightbox" href="./img/cliente6.jpg"><i class="fa fa-search"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- /Work -->
+
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- /Portfolio -->
+		
+
+	<!-- Contact -->
+	<div id="contact" class="section md-padding bg-dark">
+	
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Section-header -->
+				<div class="section-header text-center">
+					<h2 class="title" style="color: #FAFAFA">Fale Conosco</h2>
+					<h4>Funcionamento Seg a Sáb das 08 às 16h</h4>
+				</div>
+				<!-- /Section-header -->
+
+				<!-- contact -->
+				<div class="col-sm-4">
+					<div class="contact">
+						<a href="https://api.whatsapp.com/send?phone=5571987984961" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+						<h3>Mensagem Whatsapp</h3>
+						<p>(71) 9 8798-4961</p>
+					</div>
+				</div>
+				<!-- /contact -->
+
+				<!-- contact -->
+				<div class="col-sm-4">
+					<div class="contact">
+						<a href="tel: 55-71-996579885" target="_blank"><i class="fa fa-mobile"></i></a>
+						<h3>Faça uma ligação</h3>
+						<p>(71) 9 9657-9885</p>
+					</div>
+				</div>
+				<!-- /contact -->
+
+				<!-- contact -->
+				<div class="col-sm-4">
+					<div class="contact">
+						<a href="mailto:lavoaaseco@gmail.com?Subject=Hello%20again" target="_blank"><i class="fa fa-envelope"></i></a>
+						<h3>Enviar email</h3>
+						<p>lavoaaseco@gmail.com</p>
+					</div>
+				</div>
+				<!-- /contact -->
+
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+	<!-- /Contact -->
+
+
+	<!-- Footer -->
+	<footer id="footer" class="sm-padding">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<div class="col-md-12">
+
+					<!-- footer logo -->
+					<div class="footer-logo">
+						<a href="#home"><img src="img/logo1.png" alt="logo"></a>
+					</div>
+					<!-- /footer logo -->
+					
+					<ul class="footer-follow">
+						<li><a href="https://www.facebook.com/lavoasec" target="_blank"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="https://www.instagram.com/lavoasec/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+						<!-- <li><a href="" target="_blank"><i class="fa fa-youtube"></i></a></li>
+					</ul> -->
+
+					<!-- footer copyright -->
+					<div class="footer-copyright">
+						<p>A ` Sec © 2020.</p>
+					</div>
+					<!-- /footer copyright -->
+
+				</div>
+
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</footer>
+	<!-- /Footer -->
+
+
+	<!-- Preloader -->
+	<div id="preloader">
+		<div class="preloader">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</div>
+	<!-- /Preloader -->
+
+
+	<!-- jQuery Plugins -->
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="js/jquery.magnific-popup.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+
+
+</body>
+
+
+</html>
+
